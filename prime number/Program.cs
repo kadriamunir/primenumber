4120cecc47cc9cab0b4b50cb960c52e99a166066
kadriamunir <121021383+kadriamunir@users.noreply.github.com>
2022-12-19 12:24:40 +0200

    Add files via upload

---
+++ prime number/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter first number : ");
+            int k1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("piease enter second number : ");
+            int k2 = int.Parse(Console.ReadLine());
+
+            bool prime = true;
+            Console.WriteLine("the prime number are : ");
+
+            for (int x = k1; x <= k2; x++)
+            {
+                for(int y=2; y<k2; y++)
+
+                {
+                    if (x != y && x % y == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                    if (x == 1)
+                        prime = false;
+                }
+                if (prime)
+                    Console.WriteLine(x);
+                prime = true;
+
+            }
+             
+        }
+    }
+}
